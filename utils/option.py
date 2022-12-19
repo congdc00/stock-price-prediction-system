8d34196c295b25f72e68227b83fbdc84a5a202f8
CongDC <dinhchicongf9@gmail.com>
2022-12-19 16:20:42 +0700

    add poetry

--- utils/option.py
+++ utils/option.py
@@ -5,5 +5,4 @@ class Option():
             id = "ContentPlaceHolder1_ctl03_rptData2" + "_itemTR_" + str(i)
         else:
             id = "ContentPlaceHolder1_ctl03_rptData2" + "_altitemTR_" + str(i)
-
         return id
